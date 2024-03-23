0a99092868d82159b5245f67632bf9779fa769ee
willber10 <willberschickler@gamil.com>
2024-03-23 15:23:51 -0700

    added put and get requests in the database,js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,25 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log(content);
+  const jateDB = await openDB('jate', 1);
+  const tx = jateDB.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.add({ content });
+  const result = await request;
+  console.log('Data added to jate:', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const jateDB = await openDB('jate', 1);
+  const tx = jateDB.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('Data retrieved from jate:', result);
+  return result;
+};
 
 initdb();
